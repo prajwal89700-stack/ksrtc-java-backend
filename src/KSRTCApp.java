194f6209f7c6bbdb1a3f03e715504a544d11dfd9
Prajwal Kumar K <prajwal89700@gmail.com>
2026-09-15 15:16:57 +0530

    feat: implemented dynamic ArrayList and Admin routing

--- src/KSRTCApp.java
+++ src/KSRTCApp.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 public class KSRTCApp {
-    public static BusRoute findBus(BusRoute[] fleet, String targetStart, String targetDest) {
-        for (int i = 0; i < fleet.length; i++) {
-            if (fleet[i].getStartCity().equals(targetStart) && fleet[i].getDestinationCity().equals(targetDest)) {
-                return fleet[i];
+    public static BusRoute findBus(ArrayList<BusRoute> fleet, String targetStart, String targetDest) {
+        for (int i = 0; i < fleet.size(); i++) {
+            if (fleet.get(i).getStartCity().equals(targetStart) && fleet.get(i).getDestinationCity().equals(targetDest)) {
+                return fleet.get(i);
             }
         }
         return null;
@@ -14,40 +15,56 @@ public class KSRTCApp {
         BusRoute b2 = new BusRoute("Bengaluru", "Davanagere", 38, 40, 480);
         BusRoute b3 = new BusRoute("Davangere", "Bengaluru", 15, 30, 480);
         BusRoute b4 = new BusRoute("Bengaluru", "Harihar", 15, 30, 500);
-        BusRoute[] ksrtcFleet = new BusRoute[4];
-        ksrtcFleet[0] = b1;
-        ksrtcFleet[1] = b2;
-        ksrtcFleet[2] = b3;
-        ksrtcFleet[3] = b4;
+        ArrayList<BusRoute> ksrtcFleet = new ArrayList<>();
+        ksrtcFleet.add(b1);
+        ksrtcFleet.add(b2);
+        ksrtcFleet.add(b3);
+        ksrtcFleet.add(b4);
         Scanner input = new Scanner(System.in);
         while (true) {
-            System.out.println("Type 'book' to buy tickets, 'cancel' to return tickets, or 'exit' to quit:");
+            System.out.println("Type 'book', 'cancel', 'admin' to add a route, or 'exit' to quit:");
             String command = input.nextLine();
             if(command.equalsIgnoreCase("exit")){
                 System.out.println("Bye");
                 break;
-            }
-            System.out.println("Enter Start City: ");
-            String userStartCity = input.nextLine();
-            System.out.println("Enter Destination City: ");
-            String userDestinationCity = input.nextLine();
-            BusRoute myBus = findBus(ksrtcFleet, userStartCity, userDestinationCity);
-            if (myBus != null) {
-                if (command.equalsIgnoreCase("book")) {
-                    System.out.println("How many tickets do you want to book?");
-                    int userSeats = input.nextInt();
-                    myBus.bookSeats(userSeats);
-                    input.nextLine();
-                } else if (command.equalsIgnoreCase("cancel")) {
-                    System.out.println("How many tickets do you want to cancel?");
-                    int userSeats = input.nextInt();
-                    myBus.cancelSeat(userSeats);
-                    input.nextLine();
-                }else {
-                    System.out.println("Invalid command. Please try again.");
+            }else if(command.equalsIgnoreCase("admin")) {
+                System.out.println("Enter start city");
+                String startCity = input.nextLine();
+                System.out.println("Enter destination city");
+                String destinationCity = input.nextLine();
+                System.out.println("How many seats you would like to add?");
+                int numberOfSeats = input.nextInt();
+                System.out.println("Ticket Price: ");
+                int ticketPrice = input.nextInt();
+                input.nextLine();
+
+                BusRoute newBus = new BusRoute(startCity, destinationCity, numberOfSeats, numberOfSeats, ticketPrice);
+                ksrtcFleet.add(newBus);
+                System.out.println("SUCCESS: New bus added to the fleet!");
+            }else if(command.equalsIgnoreCase("book") || command.equalsIgnoreCase("cancel")){
+                System.out.println("Enter start city");
+                String startCity = input.nextLine();
+                System.out.println("Enter destination city");
+                String destinationCity = input.nextLine();
+                BusRoute myBus = findBus(ksrtcFleet, startCity, destinationCity);
+
+                if(myBus !=null){
+                    if(command.equalsIgnoreCase("book")) {
+                        System.out.println("How many tickets do you want to book?");
+                        int numberOfTickets = input.nextInt();
+                        myBus.bookSeats(numberOfTickets);
+                        input.nextLine();
+                    }else if(command.equalsIgnoreCase("cancel")){
+                        System.out.println("How many tickets do you want to cancel?");
+                        int numberOfTickets = input.nextInt();
+                        myBus.cancelSeat(numberOfTickets);
+                        input.nextLine();
+                    }
+                } else {
+                    System.out.println("FAILURE: Route does not exist. Please check your cities.");
                 }
             } else {
-                System.out.println("FAILURE: Route does not exist. Please check your cities.");
+                System.out.println("Invalid command. Please try again.");
             }
         }
     }
